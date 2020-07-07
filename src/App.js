d9d1915ea12ec0a40939f4a044cfb0bc98a17d2d
Olaf Klöcker <npmokl@gmx.com>
2020-07-07 12:18:58 +0100

    feat: update App styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LineWidthChooser from './LineWidthChooser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background: "black", padding: "3ex"}}>
+          <LineWidthChooser background="#ffffff90"/>
     </div>
   );
 }
